0fe48b996373e3bc59da236393c33ba263234e1e
Vishal Tanwar <vshal@authbridge.com>
2020-08-07 20:46:41 +0530

    Refactor

--- f2b64_and_back.py
+++ f2b64_and_back.py
@@ -2,12 +2,14 @@ import os
 import sys
 import base64
 import argparse
+from typing import Union
 
 import filetype
 
 # Module Level Constants
 EMPTY_FILE = 'EMPTY FILE'
 FILE_NOT_FOUND = 'FILE NOT FOUND'
+INCORRECT_FLAG = 'INCORRECT FLAG SPECIFIED'
 
 
 class F2B64AndBack(object):
@@ -15,57 +17,73 @@ class F2B64AndBack(object):
     Utility for Converting Files to Base64 Strings and Vice-Versa
     """
 
-    @classmethod
-    def from_file2b64(cls, file_path: str) -> str:
+    @staticmethod
+    def from_file2b64(filepath: str, **kwargs: dict) -> Union[str, None]:
         """Convert File to Base64 String"""
 
         try:
-            with open(file_path, 'rb') as f:
+            with open(filepath, 'rb') as f:
                 file_data = f.read()
                 if not file_data:
-                    sys.exit(EMPTY_FILE)
+                    return
                 b64_data = base64.b64encode(file_data).decode()
         except Exception as exc:
             tc, te, tb = sys.exc_info()
             print(f'CLASS: {tc}, ERROR: {exc}, LINE_NO: {tb.tb_lineno}')
             b64_data = ''
+
         return f'{b64_data}\n'
 
-    @classmethod
-    def from_b642file(cls, file_path_bytes: str, file_name: str) -> str:
+    @staticmethod
+    def from_b642file(filepath: str, outputfile: str, **kwargs: dict) -> Union[str, None]:
         """Convert Base64 String to File"""
 
         try:
-            file_dir = os.path.dirname(file_path_bytes)
-            with open(file_path_bytes, 'r') as f:
+            file_dir = os.path.dirname(filepath)
+            with open(filepath, 'r') as f:
                 file_data = f.read()
                 if not file_data:
-                    sys.exit(EMPTY_FILE)
+                    return
                 file_bytes = base64.b64decode(file_data)
                 ext = filetype.guess_extension(file_bytes)
 
-            output_file_path = os.path.join(file_dir, f'{file_name}.{ext}')
+            output_file_path = os.path.join(file_dir, f'{outputfile}.{ext}')
+
             with open(output_file_path, 'wb') as f:
                 f.write(file_bytes)
         except Exception as exc:
             tc, te, tb = sys.exc_info()
             print(f'CLASS: {tc}, ERROR: {exc}, LINE_NO: {tb.tb_lineno}')
             output_file_path = ''
+
         return f'{output_file_path}\n'
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Utility for Converting Files to Base64 Strings and Vice-Versa')
-    parser.add_argument('flag', help='Set 0 for F2B and 1 for B2F', type=int)
+    parser.add_argument('flag', help='Set 0 for F2B64 and 1 for B642F', type=int)
     parser.add_argument('filepath', help='Raw File/File containing Base64 String', type=str)
-    parser.add_argument('--output', '-o', help='Output filename', default='b64decoded', type=str)
+    parser.add_argument('--outputfile', '-of', help='Output filename', default='b64decoded', type=str)
+
     args = parser.parse_args()
+
     if not os.path.exists(args.filepath):
         sys.exit(FILE_NOT_FOUND)
-    if args.flag == 0:
-        data = F2B64AndBack.from_file2b64(file_path=args.filepath)
-        sys.stdout.write(data)
-    if args.flag == 1:
-        data = F2B64AndBack.from_b642file(file_name=args.output, file_path_bytes=args.filepath)
-        sys.stdout.write(data)
+
+    f2b64_and_back = F2B64AndBack()
+
+    args_func_mapping = {
+        0: f2b64_and_back.from_file2b64,
+        1: f2b64_and_back.from_b642file,
+    }
+    flag = args.flag
+
+    if flag not in args_func_mapping:
+        sys.exit(INCORRECT_FLAG)
+    else:
+        data = args_func_mapping[flag](**vars(args))
+        if data:
+            sys.stdout.write(data)
+        else:
+            sys.exit(EMPTY_FILE)
